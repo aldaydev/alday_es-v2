3c1595fcb3e3a03153878ea7e14270a7acd451ad
aldaydev <aldaydev@gmail.com>
2025-07-27 18:09:04 +0200

    chore/routes finished

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { Videos } from './pages/videos/videos';
+import { Bio } from './pages/bio/bio';
+import { NotFound } from './pages/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'trabajos', component: Projects},
+    {path: 'videos', component: Videos},
+    {path: 'bio', component: Bio},
+    {path: '**', component: NotFound}
+];
